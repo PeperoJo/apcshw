dc846cde9752383431297975753a45b939eb6c86
josephhan11 <sjhan123@gmail.com>
2014-09-17 08:58:50 -0400

    trial1

---
+++ 140917/Greeter.java
@@ -0,0 +1,9 @@
+public class Greeter {
+    public String greeting = new String("Hello world!");
+    public void greet() {
+	System.out.println(greeting);
+    }
+    public void ungreet() {
+	System.out.println("I'm out!");
+    }
+}
