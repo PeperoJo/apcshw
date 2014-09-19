85a80bcf3f86cbee2effda28edbf8eb2978c2c5d
josephhan11 <sjhan123@gmail.com>
2014-09-19 09:11:36 -0400

    test

---
+++ notes/note140915.java
@@ -0,0 +1,20 @@
+/*
+in java, all variables must be declared before use.
+declarations specify the type of the variable.
+
+local variable: declared in a method
+it is created when the function is called
+destroyed when the function exits and is only viviable (usaeabl) inside
+*/
+
+import java.io*;
+import java.util.*;
+
+public class driver {
+public static void main(String[] args){
+           /*
+           declares a local variable g to be of type Greeter
+           */
+           Greeter g;
+     }
+}
