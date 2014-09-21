3a493a288992b4667a55dcdbf412cb410d2feea1
josephhan11 <sjhan123@gmail.com>
2014-09-21 19:05:24 -0400

    homeworkthree

---
+++ 03-strings/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args){
+        String s="joseph han";
+	String first="joseph";
+	String last="han";
+	System.out.print("The space is in the ");
+	System.out.print(s.indexOf( ' ' )+1);
+	System.out.println(" place.");
+        // you'll add code here
+    }
+}
