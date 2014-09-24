e2336d996fb6a2831db134e426010fcb78615160
josephhan11 <sjhan123@gmail.com>
2014-09-24 09:26:45 -0400

    addition

--- 05-codingbat/Driver.java
+++ 05-codingbat/Driver.java
@@ -22,5 +22,28 @@ public class Driver {
 	System.out.println("diff21(21) == " + s.diff21(21));
 	System.out.println("diff21(42) == " + s.diff21(42));
 	System.out.println();
+
+	//nearHundred
+	System.out.println(s.nearHundred(80));
+	System.out.println(s.nearHundred(96));
+	System.out.println(s.nearHundred(121));
+	System.out.println(s.nearHundred(156));
+	System.out.println(s.nearHundred(207));
+
+	//TeaParty
+	System.out.println(s.teaParty(3,7));
+	System.out.println(s.teaParty(3,2));
+	System.out.println(s.teaParty(11,5));
+	System.out.println(s.teaParty(6,8));
+	System.out.println(s.teaParty(11,5));
+
+	//lastDigit
+	System.out.println(s.lastDigit(11,5,21));
+	System.out.println(s.lastDigit(11,5,21));
+	System.out.println(s.lastDigit(23,33,43));
+	System.out.println(s.lastDigit(11,5,55));
+	System.out.println(s.lastDigit(11,5,20));
+
+
     }
 }
