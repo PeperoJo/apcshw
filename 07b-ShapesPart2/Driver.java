40dda561a48a65197b342e044ced699f6ffe834c
josephhan11 <sjhan123@gmail.com>
2014-10-02 09:26:37 -0400

    changes

--- 07b-ShapesPart2/Driver.java
+++ 07b-ShapesPart2/Driver.java
@@ -3,8 +3,12 @@ public class Driver {
 	Shapes s =  new Shapes();
 	System.out.println(s.tri1(1));
 	System.out.println(s.tri1(17));
-	System.out.println(s.tri2(1));
-	System.out.println(s.tri2(17));
+	System.out.println(s.tri3(1));
+	System.out.println(s.tri3(17));
+	System.out.println(s.tri3reverse(1));
+	System.out.println(s.tri3reverse(17));
+	System.out.println(s.tri4(1));
+	System.out.println(s.tri4(17));
 
 
     }
--- 07b-ShapesPart2/Shapes.java
+++ 07b-ShapesPart2/Shapes.java
@@ -32,4 +32,65 @@ public class Shapes {
 	}
         return s;
     }
+
+    public String tri3(int h) {
+	String s = "";
+	int rowcounter = 1;
+	while (rowcounter<=h) {
+	    int spacecounter = rowcounter;
+	    int columncounter = 1;
+	    while (spacecounter+1 <= h){
+		s += " ";
+		spacecounter += 1;
+	    }
+	    while (columncounter<=(2*rowcounter-1)){
+		s += "*";
+		columncounter += 1;
+	    }
+	    s=s+"\n";
+	    rowcounter += 1;
+	}
+        return s;
+    }
+
+    public String tri3reverse(int h){
+     	String s = "";
+	int rowcounter = 1;
+	while (rowcounter<=h) {
+	    int spacecounter = rowcounter;
+	    int columncounter = 1;
+	    while (spacecounter+1 <= h){
+		s += " ";
+		spacecounter += 1;
+	    }
+	    while (columncounter<=(2*rowcounter-1)){
+		s += "*";
+		columncounter += 1;
+	    }
+	    s=s+"\n";
+	    rowcounter += 1;
+	}
+        return s;
+    }
+
+    public String tri4(int h) {
+	String s = "";
+	int rowcounter = h;
+	while (rowcounter>0) {
+	    int spacecounter = rowcounter;
+	    int columncounter = 1;
+	    while (spacecounter+1 <= h){
+		s += " ";
+		spacecounter += 1;
+	    }
+	    while (columncounter<=rowcounter){
+		s += "*";
+		columncounter += 1;
+	    }
+	    s=s+"\n";
+	    rowcounter--;
+	}
+        return s;
+    }
+    
 }
