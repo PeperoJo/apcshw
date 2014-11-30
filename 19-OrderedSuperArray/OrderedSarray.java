b97c941589b09b58a7638643a2c57312fb83c710
josephhan11 <sjhan123@gmail.com>
2014-11-30 18:20:11 -0500

    Update OrderedSarray.java

--- 19-OrderedSuperArray/OrderedSarray.java
+++ 19-OrderedSuperArray/OrderedSarray.java
@@ -1,54 +1,76 @@
 public class OrderedSarray {
-    String[] data;
-    int   last;
+    private String[] data;
+    private int nextspace;
+    private Random rnd;
 
-    public Sarray(int[] d) {
-	data = d;
+    //Constructor
+    public Sarray(int i) {
+	data = new String[i];
+	nextspace = 0;
     }
 
-    public boolean add(string i){
-        String[] data2 =  new String[data.length + 1];
-	for(int a= 0; a<length.data; a++){
-	    data2[a] = data[a];
+    //Helper Methods
+    public String printArray(String[] StringArray) {
+	String s = "{";
+	for (int i = 0; i < StringArray.length; i++){
+	    s = s + StringArray[i] + ", ";
 	}
-	data2[length.data+1] = i;
-	int[] data = new int[] data2;
+	return s.substring(0,s.length() - 2) + "}";
+    }
+
+    public void add1index() {
+	String[] data2 = new String[data.length + 1];
+	for (int index = 0; index < data.length; index++) {
+	    data2[index] = data[index];
+	}
+	data = data2;
+    }
+
+    //Main Methods
+    public boolean add(String s) {
+	if (data[data.length - 1] != null){
+	    add1index();
+	}
+	while (data[nextBlank] != null) {
+	    nextspace++;
+	}
+	data[nextspace] = s;
+	nextspace++;
 	return true;
     }
 
-    public void  add(int index, int i){
-	int[] data2 = int[data.length + 1];
-	for(int a=0; a < index; a++){
-	    data2[a] = data[a];
+    public String get(int index) {
+	if (index > data.length - 1 || index < 0) {
+	    throw new ArrayIndexOutOfBoundsException();
 	}
-	data2[index] = i;
-	for(int a=index; a < data.length; a++){
-	    data2[a+1] = data[a];
-	}
-	int[] data = new int[] data2;
+        return data[index];
     }
 
+    public String set(int index, String s) {
+	if (index > data.length - 1 || index < 0) {
+	    throw new ArrayIndexOutOfBoundsException();
+	} 
+	String oldVal = get(index);
+	data[index] = s;
+	return oldVal;
+    }
+    
     public int size() {
         return data.length;
     }
-
-    public int get(int index) {
-	return data[index];
-    }
-
-    public int set(int index, int i){
-        return data[index];
-	data[index]=i;
-    }
-
-    public int remove(int index){
-	    String[] data2 = String[data.length - 1];
-        for(int a=0; a < index; a++){
-            data2[a] = data[a];
-	}
-        for(int a=index; a < data.length-1; a++){
-            data2[a-1] = data[a];
-        }
-        int[] data = new int[] data2;
-    }
+     public static void main(String[] args){
+	Sarray s = new Sarray(10);
+	System.out.println(s.printArray(s.data));
+	s.add("one");
+	s.add("two");
+	s.add("three");
+	s.add("four");
+	s.add("five");
+	s.add("six");
+	s.add("seven");
+	s.add("eight");
+	s.add("nine");
+	s.add("ten");
+	System.out.println(s.printArray(s.data));
+     }
 }
