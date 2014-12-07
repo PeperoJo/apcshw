b0226becfd356903e7479f99e4ff12d4207ed397
josephhan11 <sjhan123@gmail.com>
2014-12-07 02:05:23 -0500

    bub bub bubble

--- 20-Sorting/Sarray.java
+++ 20-Sorting/Sarray.java
@@ -76,5 +76,22 @@ public class Sarray {
             }
       }
 }
+public void bubbleSort(int[] a) {
+      boolean swapped = true;
+      int j = 0;
+      int tmp;
+      while (swapped) {
+            swapped = false;
+            j++;
+            for (int i = 0; i < a.length - j; i++) {                                       
+                  if (a[i] > a[i + 1]) {                          
+                        tmp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = tmp;
+                        swapped = true;
+                  }
+            }                
+      }
+}
 
 }
