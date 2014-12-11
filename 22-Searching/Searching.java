83024589c0b44e2ef947509b12769ce5a42f971e
josephhan11 <sjhan123@gmail.com>
2014-12-11 09:27:06 -0500

    update

--- 22-Searching/Searching.java
+++ 22-Searching/Searching.java
@@ -1,1 +1,14 @@
-public class Searching
+public class Searching{
+    private Random r = new Random();
+    
+    public Searching{
+	int a = r.nextInt(100);
+    }
+    public Searching(int a){
+	for(int i=0; i<100; i++){
+	
+	}
+
+    }
+
+}
